0174f9fc12ececbba761f9d099fa23cd93f44d2d
Alvin Nguyen <alvinnguyen116@gmail.com>
2017-06-07 14:06:50 -0700

    Update Marker_Clusterer.js

--- js/Marker_Clusterer.js
+++ js/Marker_Clusterer.js
@@ -1253,7 +1253,7 @@ ClusterIcon.prototype.createCss = function(pos) {
 
   style.push('cursor:pointer; top:' + pos.y + 'px; left:' +
       pos.x + 'px; color:' + txtColor + '; position:absolute; font-size:' +
-      txtSize + 'px; font-family:Arial,sans-serif; font-weight:bold');
+      txtSize + 'px; font-family:Gotham-Book, Arial,sans-serif; font-weight:bold');
   return style.join('');
 };
 
