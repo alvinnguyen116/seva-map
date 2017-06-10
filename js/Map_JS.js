4e659f556c5de60e9b513cf901646236c5ab8f4b
Alvin Nguyen <alvinnguyen116@gmail.com>
2017-06-09 17:47:30 -0700

    Update Map_JS.js

--- js/Map_JS.js
+++ js/Map_JS.js
@@ -279,16 +279,10 @@ function plotMarkers(m)
     bounds.extend(position);
   });
   map.fitBounds(bounds);
+
+
   var legend = document.getElementById('legend'); //legend 
-  for (var key in icons) {
-    var type = icons[key];
-    var name = type.name;
-    var icon = type.icon;
-    var div = document.createElement('div');
-    div.innerHTML = '<img src="' + icon + '"> ' + name;
-    legend.appendChild(div);
-  }
- map.controls[google.maps.ControlPosition.LEFT_BOTTOM].push(legend);
+  map.controls[google.maps.ControlPosition.LEFT_BOTTOM].push(legend);
 }
 
 function infoWindowContent(name, content, image) {
