532f276359f60138874f252b1bb5769361958d41
Alvin Nguyen <alvinnguyen116@gmail.com>
2017-07-20 12:50:52 -0700

    Update Map_JS.js

--- js/Map_JS.js
+++ js/Map_JS.js
@@ -318,7 +318,7 @@ function initMap()
             document.getElementById("change_arrow").innerHTML = "&#187;";
       } else {
             $("#box").animate({ "margin-left": 1 }, "slow");
-            $(".legend").animate({"width": 440}, "slow");
+            $(".legend").animate({"width": 412}, "slow");
             $("#change_arrow").css({"right": 9});
             document.getElementById("change_arrow").innerHTML = "&#171;";
       }
@@ -520,7 +520,7 @@ function legend() { //toggles legend horizontally
             help_close();
       } else {
             $("#box").animate({ "margin-left": 1 }, "slow");
-            $(".legend").animate({ "width": 440}, "slow");
+            $(".legend").animate({ "width": 412}, "slow");
             $("#change_arrow").css({"right": 9});
             document.getElementById("change_arrow").innerHTML = "&#171;";
       }
@@ -597,7 +597,7 @@ function display_file(file, name) {
 
 function help_open() {
   if (getOrientation() == "Landscape") {
-    $("#legend_help").css({"height": "0", "width": "416px"});
+    $("#legend_help").css({"height": "0", "width": "426px"});
     $("#legend_help").animate({
         'height': '70px',
         'opacity': '1'
