83d211a0cea3b0e0bd34be7023c6f8512bb81c5c
Alvin Nguyen <alvinnguyen116@gmail.com>
2017-07-20 12:26:41 -0700

    Update Map_JS.js

--- js/Map_JS.js
+++ js/Map_JS.js
@@ -597,7 +597,7 @@ function display_file(file, name) {
 
 function help_open() {
   if (getOrientation() == "Landscape") {
-    $("#legend_help").css({"height": "0", "width": "426px"});
+    $("#legend_help").css({"height": "0", "width": "416px"});
     $("#legend_help").animate({
         'height': '70px',
         'opacity': '1'
