88efb6d00c6fed1fe730d86ad334efda9e4930f5
Alvin Nguyen <alvinnguyen116@gmail.com>
2017-07-20 22:20:40 -0700

    Update Map_JS.js

--- js/Map_JS.js
+++ js/Map_JS.js
@@ -603,7 +603,7 @@ function help_open() {
         'opacity': '1'
     }, 400);
   } else {
-    $("#legend_help").css({"height": "94px", "width": "0"});
+    $("#legend_help").css({"height": "95px", "width": "0"});
     $("#legend_help").animate({
         'width': '160px',
         'opacity': '1'
